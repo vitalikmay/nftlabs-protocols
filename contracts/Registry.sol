57a9e7bdf8e3705903882e88f33b90346f501469
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2021-12-22 07:40:22 +0000

    forge test registry

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -148,3 +148,77 @@ contract Registry is Ownable {
         return _protocolControls[_deployer].latestVersion;
     }
 }
+
+interface Vm {
+    // Set block.timestamp (newTimestamp)
+    function warp(uint256) external;
+
+    // Set block.height (newHeight)
+    function roll(uint256) external;
+
+    // Loads a storage slot from an address (who, slot)
+    function load(address, bytes32) external returns (bytes32);
+
+    // Stores a value to an address' storage slot, (who, slot, value)
+    function store(
+        address,
+        bytes32,
+        bytes32
+    ) external;
+
+    // Signs data, (privateKey, digest) => (r, v, s)
+    function sign(uint256, bytes32)
+        external
+        returns (
+            uint8,
+            bytes32,
+            bytes32
+        );
+
+    // Gets address for a given private key, (privateKey) => (address)
+    function addr(uint256) external returns (address);
+
+    // Performs a foreign function call via terminal, (stringInputs) => (result)
+    function ffi(string[] calldata) external returns (bytes memory);
+
+    // Calls another contract with a specified `msg.sender`, (newSender, contract, input) => (success, returnData)
+    function prank(
+        address,
+        address,
+        bytes calldata
+    ) external payable returns (bool, bytes memory);
+
+    // Sets an address' balance, (who, newBalance)
+    function deal(address, uint256) external;
+
+    // Sets an address' code, (who, newCode)
+    function etch(address, bytes calldata) external;
+
+    // Expects an error on next call
+    function expectRevert(bytes calldata) external;
+}
+
+contract RegistryTest {
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    Registry registry;
+
+    function setUp() public {
+        registry = new Registry(address(0), address(0), address(0));
+    }
+
+    function testSetTreasury(address addy) public {
+        registry.setTreasury(addy);
+        require(registry.treasury() == addy);
+    }
+
+    function testSetDefaultFeeBps() public {
+        registry.setDefaultFeeBps(0);
+        require(registry.defaultFeeBps() == 0);
+        registry.setDefaultFeeBps(registry.MAX_PROVIDER_FEE_BPS());
+        require(registry.defaultFeeBps() == registry.MAX_PROVIDER_FEE_BPS());
+    }
+
+    function testFailSetDefaultFeeBps() public {
+        registry.setDefaultFeeBps(registry.MAX_PROVIDER_FEE_BPS() + 1);
+    }
+}
